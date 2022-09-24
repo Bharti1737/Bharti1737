32f1b2dcab7185b47c0486cd68028f87c0211785
Bharti1737 <114172904+Bharti1737@users.noreply.github.com>
2022-09-24 14:23:55 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int i,sum=0;
+	    for( i=1;i<=50;i++)
+	    {
+	        sum=sum+i;
+	    }
+		System.out.println("sum="+sum);
+	}
+}
